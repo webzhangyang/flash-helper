ad54e42e051e886a52c8e2a244e72dc5f6451225
test <zhangyang@misrobot.com>
2019-11-13 23:18:14 +0800

    增加test页面，再次协商hello-world

--- .temp/app.js
+++ .temp/app.js
@@ -1,8 +1,7 @@
-import Nerv from "nervjs";
 import '@tarojs/async-await';
 import Taro, { Component } from "@tarojs/taro-h5";
 import { Provider } from "@tarojs/redux-h5";
-import Index from "./pages/index/index";
+
 import configStore from "./store/index";
 import './app.less';
 // 如果需要在 h5 环境中开启 React Devtools
@@ -10,6 +9,25 @@ import './app.less';
 // if (process.env.NODE_ENV !== 'production' && process.env.TARO_ENV === 'h5')  {
 //   require('nerv-devtools')
 // }
+import Nerv from "nervjs";
+import { Router, createHistory, mountApis } from '@tarojs/router';
+Taro.initPxTransform({
+  "designWidth": 750,
+  "deviceRatio": {
+    "640": 1.17,
+    "750": 1,
+    "828": 0.905
+  }
+});
+
+const _taroHistory = createHistory({
+  mode: "hash",
+  basename: "/",
+  customRoutes: {},
+  firstPagePath: "/pages/index/index"
+});
+
+mountApis(_taroHistory);
 const store = configStore();
 class App extends Component {
   constructor() {
@@ -21,8 +39,11 @@ class App extends Component {
      * 对于像 navigationBarTextStyle: 'black' 这样的推导出的类型是 string
      * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
      */
+    Taro._$app = this;
   }
-  componentDidMount() {}
+  componentDidMount() {
+    this.componentDidShow();
+  }
   componentDidShow() {}
   componentDidHide() {}
   componentDidCatchError() {}
@@ -30,11 +51,21 @@ class App extends Component {
   // 请勿修改此函数
   render() {
     return <Provider store={store}>
-        <Index />
-      </Provider>;
+                  
+              <Router history={_taroHistory} routes={[{
+        path: '/pages/index/index',
+        componentLoader: () => import( /* webpackChunkName: "index_index" */'./pages/index/index'),
+        isIndex: true
+      }, {
+        path: '/pages/test/test',
+        componentLoader: () => import( /* webpackChunkName: "test_test" */'./pages/test/test'),
+        isIndex: false
+      }]} customRoutes={{}} />
+              
+                </Provider>;
   }
   config = {
-    pages: ['pages/index/index'],
+    pages: ["/pages/index/index", "/pages/test/test"],
     window: {
       backgroundTextStyle: 'light',
       navigationBarBackgroundColor: '#fff',
@@ -42,5 +73,10 @@ class App extends Component {
       navigationBarTextStyle: 'black'
     }
   };
+
+  componentWillUnmount() {
+    this.componentDidHide();
+  }
+
 }
-Taro.render(<App />, document.getElementById('app'));
+Nerv.render(<App />, document.getElementById('app'));
--- README.md
+++ README.md
@@ -1,11 +1,16 @@
 # Flash Helper
 ### for learning Taro
 
-## 基于京东凹凸实验室Taro框架，配置有：react、reduce、less、eslint、typescript
+## 基于京东凹凸实验室Taro框架，配置有：react、redux、less、eslint、typescript
+
+## 本地环境各种页面地址，如test页面，就是http://127.0.0.1:10086/#/pages/test/test;页面配置在app.tsx中。
 
 ## 2019-11-06
 - 建立dev分支，开始：hello world
 
 ## 2019-11-07
-- 重建项目，使用reduce和typescript
+- 重建项目，使用redux和typescript
+
+## 2019-11-13
+- 新建页面test，专注于测试的页面
 

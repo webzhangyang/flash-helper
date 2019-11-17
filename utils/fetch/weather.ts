de00794d7711f830c82a8025c43a222050f0a6a3
test <zhangyang@misrobot.com>
2019-11-17 18:16:30 +0800

    增加获取天气的api——完善接口

--- utils/fetch/weather.ts
+++ utils/fetch/weather.ts
@@ -13,36 +13,60 @@ const pathObj = {
   }
 }
 
+// 通用的参数接口
+interface Params {
+  dtype?: string
+  format?: number
+  key?: string
+}
+
 /*
+  根据城市名获取天气
 	cityname	Y	string	城市名或城市ID，如："苏州"，需要utf8 urlencode
  	dtype	N	string	返回数据格式：json或xml,默认json
  	format	N	int	未来7天预报(future)两种返回格式，1或2，默认1
  	key	Y	string	在个人中心->我的数据,接口名称上方查看
 */
-type Params = {
+interface IndexParams extends Params{
   cityname: string
-  dtype?: string
-  format?: number
-  key?: string
 }
-export async function getWeatherByCityName(params:Params = { cityname: '深圳'}):Promise<object>{
-  let options = {
+export async function getWeatherByCityName(params:IndexParams = { cityname: '深圳'}):Promise<object>{
+  return await getWeather({
     path: 'index',
     data: {
       cityname : encodeURIComponent(params.cityname),
       ...params 
     }
-  }
-  return await getWeather(options)
+  })
 }
 
+/*
+  根据城市名获取天气
+	cityname	Y	string	城市名或城市ID，如："苏州"，需要utf8 urlencode
+ 	dtype	N	string	返回数据格式：json或xml,默认json
+ 	format	N	int	未来7天预报(future)两种返回格式，1或2，默认1
+ 	key	Y	string	在个人中心->我的数据,接口名称上方查看
+*/
+interface GPSParams extends Params{
+  lon: string // 经度
+  lat: string // 纬度
+}
+export async function getWeatherByGPS(params:GPSParams):Promise<object>{
+  return await getWeather({
+    path: 'geo',
+    data: params
+  })
+}
+
+/* 
+  封装获取天气的ajax
+*/
 type PathOptions = {
   path: string
   data?: object
   header?: object
   key?: string
 }
-
 export async function getWeather(options: PathOptions = { path: 'index',header: {'content-type': 'application/json'} }):Promise<object> {
   return await Taro.request({
     url: pathObj.getPath(options.path),
